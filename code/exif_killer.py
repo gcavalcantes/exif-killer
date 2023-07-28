27b73da53b1ad4064d778785436b3505ba22f824
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2023-07-28 19:42:48 -0300

    Removed some TODOs
    Removed useless TODOs.

--- code/exif_killer.py
+++ code/exif_killer.py
@@ -12,22 +12,26 @@ def exifKiller(file_path):
             # remove in production
             image = Image(file_image)
             print("File successfuly opened. Proceding... ")
-            checkFileType(file_path)
-            # Detects existing exif data
-            checkExif(image)
-    # TODO Erases the exif data
-    # TODO Checks if the data has been erased
-    # TODO Show message that confirms that the data has bee erased.
+            if checkFileType(file_path):
+
+                # Detects existing exif data
+                if checkExif(image):
+                    pass
+                    # TODO Erases the exif data
+                    # TODO Checks if the data has been erased
+                    # TODO Show message that confirms that the data has bee erased.
+                else:
+                    # remove in production
+                    print("Exif data could not be detected. Ending operations.")
     else:
         print("File could not be found. Ending operations.")
         pass
 
 
-# TODO Check if the file is of a valid format
+# Check if the file is of a valid format
 def checkFileType(file):
     # List of accepted extensions
-    accepted_extensions = [".png", ".jpg", ".jfif", ".jpeg", ".bmp", "", "", "", ""]
-    # TODO Checks if the file is in a valid format
+    accepted_extensions = [".png", ".jpg", ".jfif", ".jpeg", ".bmp"]
     file_name, file_extension = os.path.splitext(file)
     if file_extension in accepted_extensions :
         # remove in production
@@ -43,9 +47,13 @@ def checkExif(image):
     status = ''
     if image.has_exif:
         status = f"contains EXIF (version {image.exif_version}) information."
+        print(f"Image {status}")
+        return True
     else:
         status = "does not contain any EXIF information."
-    print(f"Image {status}")
+        print(f"Image {status}")
+        return False
+    
 
 # Testing the function
 exifKiller('/Users/Cliente/Pictures/Saved Pictures/bsg1.jpg ')
