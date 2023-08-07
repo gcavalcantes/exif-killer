358ee9280864365cd609029be3165449ee6eb6e3
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2023-08-07 15:11:22 -0300

    Kill Exif button
    Added the kill exif button.
    No functionality yet.

--- code/interface.py
+++ code/interface.py
@@ -3,19 +3,30 @@ from tkinter import *
 class Interface():
     # TODO Creates a screen for the main window
     def __init__(self, master = None):
-        self.wiget_main = Frame(master)
-        self.wiget_main.pack()
+        self.widget_main = Frame(master)
+        self.widget_main.pack()
         # Message at the top of the screen
-        self.message = Label(self.wiget_main, text="Relevant messages are shown here")
+        self.message = Label(self.widget_main, text="Relevant messages are shown here")
         self.message["font"] = ("Verdana", "10", "italic", "bold")
         self.message.pack()
+        # Kill Exif button
+        self.kill_exif = Button(self.widget_main)
+        self.kill_exif["text"] = "Kill Exif"
+        self.kill_exif["font"] = ("Verdana", "10")
+        self.kill_exif["width"] = 5
+        self.kill_exif.bind("<Button-1>", self.killExif)
+        self.kill_exif.pack(side=LEFT)
         # Exit button
-        self.exit = Button(self.wiget_main)
+        self.exit = Button(self.widget_main)
         self.exit["text"] = "Exit"
         self.exit["font"] = ("Verdana", "10")
         self.exit["width"] = 5
-        self.exit["command"] = self.wiget_main.quit
+        self.exit["command"] = self.widget_main.quit
         self.exit.pack(side=RIGHT)
+
+    # TODO call the exif killer function
+    def killExif(self, event):
+        pass
 
 root = Tk()
 Interface(root)
