9fb6c8c0638edf4a2da7b51e09c63afb5da2f6e8
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2023-08-08 15:09:44 -0300

    Menu options
    Create de menu options.
    Added the option to exit the program through the menu.

--- code/interface.py
+++ code/interface.py
@@ -12,15 +12,36 @@ class Interface():
         # Changes the window name
         master.title("Exif Killer")
         # Changes the dimensions of the window
-        master.geometry('600x400')
-        # Main frame of the program's window
+        master.geometry('600x400+50+50')
+        # Main frame of the app's window
         self.widget_main = Frame(master)
         self.widget_main.pack()
+
+        # Menu bar
+        self.menu_bar = Menu()
+        self.menu_about = Menu(self.menu_bar, tearoff=False)
+        self.menu_options = Menu(self.menu_bar, tearoff=False)
+
+        self.menu_bar.add_cascade(label='Options', menu=self.menu_options)
+        self.menu_bar.add_cascade(label='About', menu=self.menu_about)
+
+        # Display of menu bar in the app
+        master.config(menu=self.menu_bar)
+
+        # Tool bar
+        self.toolbar = Label(master)
+        self.toolbar.pack(side=TOP, fill=X)
+
+        # TODO Create the options menu items
+        self.menu_options.add_command(
+            label="Exit", command=self.widget_main.quit)
+
         # Message at the top of the screen
         self.message = Label(
             self.widget_main, text="Relevant messages are shown here")
         self.message["font"] = ("Verdana", "10", "italic", "bold")
         self.message.pack()
+
         # Kill Exif button
         self.kill_exif = Button(self.widget_main)
         self.kill_exif["text"] = "Kill Exif"
@@ -28,6 +49,7 @@ class Interface():
         self.kill_exif["width"] = 5
         self.kill_exif.bind("<Button-1>", self.killExif)
         self.kill_exif.pack(side=LEFT)
+
         # Exit button
         self.exit = Button(self.widget_main)
         self.exit["text"] = "Exit"
