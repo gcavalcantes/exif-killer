480ad75ae792a20b21d570ea2e20c81a038c9d9f
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2023-08-17 20:38:40 -0300

    About window changes
    Removed the unecessary init.
    Added the label with information about the app.

--- code/about_window.py
+++ code/about_window.py
@@ -1,6 +1,6 @@
 # This file deals with the "About" window
 
-#import tkinter acourding to the Python version
+# import tkinter acourding to the Python version
 try:
     # For Python 3
     from tkinter import *
@@ -8,8 +8,11 @@ except ImportError:
     # For Python 2
     from Tkinter import *
 
+
 class AboutWindow():
-    def __init__(self, master = None):
-        super().__init__(master = master)
-        self.title("About this program")
-        self.geometry("200:200")
+    def aboutWindow(window):
+        about = Toplevel(window)
+        about.title("About this program")
+        about.geometry("200x200")
+        version_label = Label(about, text="Exif Killer version: 0.0.1")
+        version_label.pack()
